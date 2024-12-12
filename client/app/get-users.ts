c4a1f549174af9ad1b6403ca7c62c4a9352889e8
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:37:22 -0800

    Update get-users.ts

--- client/app/get-users.ts
+++ client/app/get-users.ts
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 
-export default async function getUsers() {
+export default async function getUsers() 
+{
   const res = await fetch("http://localhost:3000", {
     headers: {
       Cookie: cookies().toString(),

e61b78e20188b6036f90d15e6c3d4cc94c5d3eea
nicholas harper <nicholasharper000@gmail.com>
2024-12-17 22:21:22 -0800

    Update app.service.ts

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@nestjs/common';
 import clerkClient from '@clerk/clerk-sdk-node';
 
 @Injectable()
-export class AppService {
-  async getUsers() {
+export class AppService 
+{
+  async getUsers() 
+  {
     return clerkClient.users.getUserList();
   }
 }
